132c3ed21a272ae85fff825d3491bd67a9701257
ksuteamwork <allasd900128@gmail.com>
2016-08-05 05:35:10 -0400

    add client2.c

--- client2.c
+++ client2.c
@@ -15,7 +15,7 @@
 #define SERVER_IP	"192.168.128.95"
 
 int main(){
-	int s;
+	int s,len;
 	struct sockaddr_in addr;
 	char buffer[1024];
 	char var[]="GET /2.txt HTTP/1.1\r\n"
@@ -25,7 +25,7 @@ int main(){
 		   "Host: 192.168.128.65\r\n"
 		   "Cpnnection: Keep-Alive\r\n\r\n";
 
-	FILE*fb=fopen("ac.txt","w");
+	FILE*fd;
 
 	if((s = socket(AF_INET, SOCK_STREAM, 0))<0){
 		perror("socket");
@@ -43,15 +43,17 @@ int main(){
 	}
 
 	sprintf(buffer,"%s" ,var);
-	write(s, buffer,sizeof(buffer));
+        rite(s, buffer,sizeof(buffer));
 	bzero(buffer, sizeof(buffer));
-	//recv(s, buffer, sizeof(buffer), 0);
-	//printf("%s\n", buffer);
 
-	int len = recv(s,buffer,sizeof(buffer),0);
-	while(len < sizeof(buffer)){
-	recv(s,buffer,sizeof(buffer),0);
-	//printf("%s\n",buffer);
-	fprintf(fb,"%s\n",buffer);
+	recv(s, buffer, sizeof(buffer), 0);
+	printf("%s\n", buffer);
+	bzero(buffer, sizeof(buffer));
+	fd = fopen("a.txt","w");
+
+	//int len = recv(s,buffer,sizeof(buffer),0);
+	while(len = recv(s,buffer,sizeof(buffer),0) > 0){
+	fprintf(fd,"%s", buffer);
+	bzero(buffer, sizeof(buffer));
 	}
 }
